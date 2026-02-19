54cacdaa84f9fe4a229929c5f1f8435cede64f7e
Codons-ensemble <bounamakountagoudiaby@gmail.com>
2026-02-19 18:00:51 +0000

    first commit 23EFGB

--- authentification/settings.py
+++ authentification/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://stock-gestion-qtpy.onrender.com",
+    "https://gestion-commercial.onrender.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
